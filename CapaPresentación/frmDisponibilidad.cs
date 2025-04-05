a0302abc400f2a59dfb8bd221fc047b89ac2560c
LUIS AMIR AQUINO MEJÍA <la2023-0685@uce.edu.do>
2025-04-05 13:39:46 -0400

    ffff

--- CapaPresentación/frmDisponibilidad.cs
+++ CapaPresentación/frmDisponibilidad.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-
+//ggggg
 namespace CapaPresentación
 {
     public partial class frmDisponibilidad : Form
